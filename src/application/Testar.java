a869ff06891728d8bc210962b4fe6deb01c01233
Carlos Alexandre <caalbopt@gmail.com>
2019-06-08 15:18:32 -0300

    Funcoes extras

--- src/application/Testar.java
+++ src/application/Testar.java
@@ -1,15 +1,28 @@
 package application;
 
-import funcoes.para_arquivos.Manipular;
+
+import java.io.File;
+
+
+
+import funcoes.para_conteudo.ManipularConteudo;
+
 
 public class Testar
 {
 
 	public static void main(String[ ] args)
 	{
-		//Manipular.criarArquivo("/home/carlos-abp/teste/","teste.txt");
-		Manipular.apagarArquivo("/home/carlos-abp/teste/","teste.txt");
+		// Manipular.criarArquivo("/home/carlos-abp/teste/","teste.txt");
+
+		// Manipular.apagarArquivo("/home/carlos-abp/teste/","teste.txt");
+		File arq = new File("/home/carlos-abp/teste/teste.txt");
+
+		//ManipularConteudo.escreverNoArquivo("Carla,27", arq, true);
 		
+		ManipularConteudo.limparOArquivo(arq);
+		
+
 	}
 
 }
